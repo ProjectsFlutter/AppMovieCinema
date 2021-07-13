314c5380da09c375aa7282b3123dc82447a7ca9c
Jorge Luis Alonso <alonso.jorgeluis.01@gmail.com>
2021-07-13 11:02:04 -0500

    HTTP requests

--- lib/providers/movies-provider.dart
+++ lib/providers/movies-provider.dart
@@ -1,11 +1,25 @@
+import 'dart:convert';
+
 import 'package:flutter/widgets.dart';
+import 'package:http/http.dart' as http;
 
 class MoviesProvider extends ChangeNotifier{
+  String _apiKey    ='6bbd742ad8dde52daa43858057ffee11';
+  String _baseUrl   ='api.themoviedb.org';
+  String _languaje  ='es-ES';
+
   MoviesProvider(){
     print('MoviesProvider init');
     this.getOnDisplayMovies();
   }
-  getOnDisplayMovies(){
-    print('getOnDisplayMovies');
+  getOnDisplayMovies() async{
+     var url = Uri.https(_baseUrl, '3/movie/now_playing', {
+       'api_key'  : _apiKey,
+       'language' : _languaje,
+       'page'     : '1'
+     });
+     final response = await http.get(url);
+     final Map<String, dynamic> decodeData = json.decode(response.body);
+     print( decodeData);
   }
 }
